8dc9ceabc02a3f6b872e3e63445c67b781bd8b02
Octavio Galland <octavio.galland@canonical.com>
2025-01-07 20:12:24 -0300

    Only attempt to reduce aslr entropy on Linux hosts

--- artifacts/prepare_host_for_fuzzing.sh
+++ artifacts/prepare_host_for_fuzzing.sh
@@ -7,6 +7,9 @@ echo core | sudo tee /proc/sys/kernel/core_pattern
 echo "Setting CPU frequency scaling governor to 'performance'..."
 cd /sys/devices/system/cpu && (echo performance | sudo tee cpu*/cpufreq/scaling_governor)
 
-echo "Running 'sysctl vm.mmap_rnd_bits=28'..."
-echo "(This is required to work around a known bug with old versions of LLVM on newer kernels, see: https://github.com/google/sanitizers/issues/1614)"
-sudo sysctl vm.mmap_rnd_bits=28
+if [ $(uname) == "Linux" ]
+then
+    echo "Running 'sysctl vm.mmap_rnd_bits=28'..."
+    echo "(This is required to work around a known bug with old versions of LLVM on newer Linux kernels, see: https://github.com/google/sanitizers/issues/1614)"
+    sudo sysctl vm.mmap_rnd_bits=28
+fi
